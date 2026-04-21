8ae57cbb0039248e28c2957dcfc337d19fd899b7
Artem <pskovsya4827@gmail.com>
2026-04-21 20:13:06 +0300

    init frontend setup (vue + tailwind + pinia)

---
+++ frontend/board-project/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+ 
+</script>
+
+<template>
+ <div class="min-h-screen flex items-center justify-center bg-gray-100">
+  <h1 class="text-5xl font-bold text-blue-600">
+    Board Project 🚀
+  </h1>
+</div>
+ 
+</template>
