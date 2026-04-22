2fdf434133553f363301d7eae8bc1091023c0fe2
banan-minon <pskovsya4827@gmail.com>
2026-04-22 23:19:52 +0300

    init frontend setup (vue + tailwind + pinia) (#3)
    * init frontend setup (vue + tailwind + pinia)
    
    * renamed the folder, connected styles and add basic page with tailwind
    
    * refactor: move board_project contents to client root
    
    ---------
    
    Co-authored-by: Zapolyarny <zapolyarnynorth@gmail.com>

---
+++ client/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-gray-100">
+    <h1 class="text-5xl font-bold text-blue-600">
+      Board Project 🚀
+    </h1>
+  </div>
+</template>
